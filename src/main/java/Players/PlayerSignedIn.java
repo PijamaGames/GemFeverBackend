276498b3c73b64930bba205c56dfbd0c41d1dc24
Pedro Casas Martínez <pedrocasas876@gmail.com>
2020-12-26 13:29:15 +0100

    evento de sign out

--- src/main/java/Players/PlayerSignedIn.java
+++ src/main/java/Players/PlayerSignedIn.java
@@ -9,20 +9,30 @@ public class PlayerSignedIn extends PlayerState {
 		super(player);
 	}
 
-	private enum FrontendEvents{};
-	private enum BackendEvents{};
+	private enum FrontendEvents{SignedOut};
+	private enum BackendEvents{SignOut};
 	
 	public void handleMessage(JsonNode inMsg) {
-		/*BackendEvents event = null;
+		BackendEvents event = null;
 		try {
 			event = BackendEvents.values()[(inMsg.get("evt").asInt())];
 		} catch(ArrayIndexOutOfBoundsException e) {
 			
-		}*/
+		}
 		
-		/*switch(event) {
-
-		}*/
+		switch(event) {
+		case SignOut:
+			signOut();
+			break;
+		}
+	}
+	
+	public void signOut() {
+		player.setState(player.connectedState);
+		player.setUser(null);
+		ObjectNode outMsg = mapper.createObjectNode();
+		outMsg.put("evt", FrontendEvents.SignedOut.ordinal());
+		player.sendMessage(outMsg.toString());
 	}
 	
 	protected void begin() {
